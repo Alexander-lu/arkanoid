c4ec6e2fdb54c7cf10b24bb0d538fecaae07e661
Alexander_Lu <81604280@qq.com>
2022-04-14 21:00:50 +0800

    最终版本

--- src/Arkanoid.java
+++ src/Arkanoid.java
@@ -1,158 +1,229 @@
 import acm.graphics.*;
-import acm.program.*;
-import acm.graphics.GLabel;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
 public class Arkanoid extends GraphicsProgram {
-    public static final int APPLICATION_WIDTH = 400;
-    public static final int APPLICATION_HEIGHT = 600;
+    /* 游戏界面的长和宽 */
+    public static final int APPLICATION_WIDTH = 620;
+    public static final int APPLICATION_HEIGHT = 800;
+    /* 定义一个randomGenerator随机生成数 */
     RandomGenerator randomGenerator = RandomGenerator.getInstance();
-
-    /* 动画每一帧间隔10ms*/
+    /* 动画每一帧间隔16ms，即一秒60帧 */
     private static final int DELAY = 16;
-    boolean c = true;
-
-    public static final Color BRICK_COLOR = Color.BLACK;
-    public static final int BRICK_WIDTH = 18;
+    /* 布尔stopGame用于判断是否结束游戏 */
+    boolean stopGame = true;
+    /* 定义砖块的长宽 */
+    public static final int BRICK_WIDTH = 25;
     public static final int BRICK_HEIGHT = 8;
     public static final int BRICK_MARGIN = 1;
+    /* 定义挡板的长宽 */
     private static final int PADDLE_WIDTH = 100;
-    private static final int PADDLE_HEIGHT = 8;
+    private static final int PADDLE_HEIGHT = 5;
     /* 初始水平速度：每一帧水平方向的移动距离 */
-    private static final double VELOCITY_Y = 5;
-    public final int A = randomGenerator.nextInt(-10, -1);
-
+    public final int LEVEL = randomGenerator.nextInt(-12, -8);
     /* 初始竖直速度：每一帧竖直方向的移动距离 */
-    private static final double VELOCITY_X = 2;
-    public final int B = randomGenerator.nextInt(-10, -1);
-public int a = 0;
+    public final int VERTICAL = randomGenerator.nextInt(-12, -8);
+    /* 定义a，用于判断是否死亡3次结束游戏 */
+    public int a = 0;
     /* 小球的半径 */
     private static final int BALL_RADIUS = 15;
-
-    /* 小球的颜色 */
-    private static final Color BALL_COLOR = Color.BLACK;
-
-    /* 小球 */
+    /* 小球 挡板 */
     GOval ball;
     GRect paddle;
-
     /* 小球此刻的水平速度 */
     double vx;
     /* 小球此刻的竖直速度 */
     double vy;
-
-    //        这里调用了函数getCollidingObject()
+    /* 得到小球碰到的物体 */
     public GObject getCollidingObject() {
-        double x = ball.getX();
-        double y = ball.getY();
-
-        double x1 = ball.getX();
-        double y1 = ball.getBottomY();
-
-        double x2 = ball.getRightX();
-        double y2 = ball.getY();
-
-        double x3 = ball.getRightX();
-        double y3 = ball.getBottomY();
-
-        if (getElementAt(x, y) != null) {
-            return getElementAt(x, y);
-        } else if (getElementAt(x1, y1) != null) {
-            return getElementAt(x1, y1);
-        } else if (getElementAt(x2, y2) != null) {
-            return getElementAt(x2, y2);
-        } else if (getElementAt(x3, y3) != null) {
-            return getElementAt(x3, y3);
-        } else {
-            return null;
+        /* getElementAt(x, y)可以获取画布上(x,y)位置的图形 */
+        GObject obj = getElementAt(ball.getX(), ball.getY());
+        GObject objx1 = getElementAt(ball.getX() + 0.1 * BALL_RADIUS, ball.getY());
+        GObject objx2 = getElementAt(ball.getX() + 0.2 * BALL_RADIUS, ball.getY());
+        GObject objx3 = getElementAt(ball.getX() + 0.3 * BALL_RADIUS, ball.getY());
+        GObject objx4 = getElementAt(ball.getX() + 0.4 * BALL_RADIUS, ball.getY());
+        GObject objx5 = getElementAt(ball.getX() + 0.5 * BALL_RADIUS, ball.getY());
+        GObject objx6 = getElementAt(ball.getX() + 0.6 * BALL_RADIUS, ball.getY());
+        GObject objx7 = getElementAt(ball.getX() + 0.7 * BALL_RADIUS, ball.getY());
+        GObject objx8 = getElementAt(ball.getX() + 0.8 * BALL_RADIUS, ball.getY());
+        GObject objx9 = getElementAt(ball.getX() + 0.9 * BALL_RADIUS, ball.getY());
+        GObject objx10 = getElementAt(ball.getX() + BALL_RADIUS, ball.getY());
+        GObject objx11 = getElementAt(ball.getX() + 1.1 * BALL_RADIUS, ball.getY());
+        GObject objx12 = getElementAt(ball.getX() + 1.2 * BALL_RADIUS, ball.getY());
+        GObject objx13 = getElementAt(ball.getX() + 1.3 * BALL_RADIUS, ball.getY());
+        GObject objx14 = getElementAt(ball.getX() + 1.4 * BALL_RADIUS, ball.getY());
+        GObject objx15 = getElementAt(ball.getX() + 1.5 * BALL_RADIUS, ball.getY());
+        GObject objx16 = getElementAt(ball.getX() + 1.6 * BALL_RADIUS, ball.getY());
+        GObject objx17 = getElementAt(ball.getX() + 1.7 * BALL_RADIUS, ball.getY());
+        GObject objx18 = getElementAt(ball.getX() + 1.8 * BALL_RADIUS, ball.getY());
+        GObject objx19 = getElementAt(ball.getX() + 1.9 * BALL_RADIUS, ball.getY());
+        GObject objx20 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY());
+        GObject objy1 = getElementAt(ball.getX(), ball.getY() + 0.1 * BALL_RADIUS);
+        GObject objy2 = getElementAt(ball.getX(), ball.getY() + 0.2 * BALL_RADIUS);
+        GObject objy3 = getElementAt(ball.getX(), ball.getY() + 0.3 * BALL_RADIUS);
+        GObject objy4 = getElementAt(ball.getX(), ball.getY() + 0.4 * BALL_RADIUS);
+        GObject objy5 = getElementAt(ball.getX(), ball.getY() + 0.5 * BALL_RADIUS);
+        GObject objy6 = getElementAt(ball.getX(), ball.getY() + 0.6 * BALL_RADIUS);
+        GObject objy7 = getElementAt(ball.getX(), ball.getY() + 0.7 * BALL_RADIUS);
+        GObject objy8 = getElementAt(ball.getX(), ball.getY() + 0.8 * BALL_RADIUS);
+        GObject objy9 = getElementAt(ball.getX(), ball.getY() + 0.9 * BALL_RADIUS);
+        GObject objy10 = getElementAt(ball.getX(), ball.getY() + BALL_RADIUS);
+        GObject objy11 = getElementAt(ball.getX(), ball.getY() + 1.1 * BALL_RADIUS);
+        GObject objy12 = getElementAt(ball.getX(), ball.getY() + 1.2 * BALL_RADIUS);
+        GObject objy13 = getElementAt(ball.getX(), ball.getY() + 1.3 * BALL_RADIUS);
+        GObject objy14 = getElementAt(ball.getX(), ball.getY() + 1.4 * BALL_RADIUS);
+        GObject objy15 = getElementAt(ball.getX(), ball.getY() + 1.5 * BALL_RADIUS);
+        GObject objy16 = getElementAt(ball.getX(), ball.getY() + 1.6 * BALL_RADIUS);
+        GObject objy17 = getElementAt(ball.getX(), ball.getY() + 1.7 * BALL_RADIUS);
+        GObject objy18 = getElementAt(ball.getX(), ball.getY() + 1.8 * BALL_RADIUS);
+        GObject objy19 = getElementAt(ball.getX(), ball.getY() + 1.9 * BALL_RADIUS);
+        GObject objy20 = getElementAt(ball.getX(), ball.getY() + 2 * BALL_RADIUS);
+        GObject obju1 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 0.1 * BALL_RADIUS);
+        GObject obju2 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 0.2 * BALL_RADIUS);
+        GObject obju3 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 0.3 * BALL_RADIUS);
+        GObject obju4 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 0.4 * BALL_RADIUS);
+        GObject obju5 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 0.5 * BALL_RADIUS);
+        GObject obju6 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 0.6 * BALL_RADIUS);
+        GObject obju7 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 0.7 * BALL_RADIUS);
+        GObject obju8 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 0.8 * BALL_RADIUS);
+        GObject obju9 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 0.9 * BALL_RADIUS);
+        GObject obju10 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + BALL_RADIUS);
+        GObject obju11 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 1.1 * BALL_RADIUS);
+        GObject obju12 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 1.2 * BALL_RADIUS);
+        GObject obju13 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 1.3 * BALL_RADIUS);
+        GObject obju14 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 1.4 * BALL_RADIUS);
+        GObject obju15 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 1.5 * BALL_RADIUS);
+        GObject obju16 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 1.6 * BALL_RADIUS);
+        GObject obju17 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 1.7 * BALL_RADIUS);
+        GObject obju18 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 1.8 * BALL_RADIUS);
+        GObject obju19 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 1.9 * BALL_RADIUS);
+        GObject obju20 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji1 = getElementAt(ball.getX() + 0.1 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji2 = getElementAt(ball.getX() + 0.2 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji3 = getElementAt(ball.getX() + 0.3 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji4 = getElementAt(ball.getX() + 0.4 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji5 = getElementAt(ball.getX() + 0.5 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji6 = getElementAt(ball.getX() + 0.6 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji7 = getElementAt(ball.getX() + 0.7 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji8 = getElementAt(ball.getX() + 0.8 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji9 = getElementAt(ball.getX() + 0.9 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji11 = getElementAt(ball.getX() + 1.1 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji12 = getElementAt(ball.getX() + 1.2 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji13 = getElementAt(ball.getX() + 1.3 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji14 = getElementAt(ball.getX() + 1.4 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji15 = getElementAt(ball.getX() + 1.5 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji16 = getElementAt(ball.getX() + 1.6 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji17 = getElementAt(ball.getX() + 1.7 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji18 = getElementAt(ball.getX() + 1.8 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        GObject obji19 = getElementAt(ball.getX() + 1.9 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+        //循环检测是否撞到小球任意四条边中的一个
+        GObject[] list = {obj, objx1, objx2, objx3,objx4,objx5,objx6,objx7,objx8,objx9,objx10,objx11,objx12,objx13,objx14,objx15,objx16,objx17,objx18,objx19,objx20,objy1, objy2, objy3,objy4,objy5,objy6,objy7,objy8,objy9,objy10,objy11,objy12,objy13,objy14,objy15,objy16,objy17,objy18,objy19,objy20,obju1, obju2, obju3,obju4,obju5,obju6,obju7,obju8,obju9,obju10,obju11,obju12,obju13,obju14,obju15,obju16,obju17,obju18,obju19,obju20,obji1, obji2, obji3,obji4,obji5,obji6,obji7,obji8,obji9,obji11,obji12,obji13,obji14,obji15,obji16,obji17,obji18,obji19};
+        for (GObject object : list) {
+            if (object != null) {
+                return object;
+            }
         }
+        return null;
+        //撞到小球任意四条边中的一个回复物体，没撞到回复null
     }
-
     /**
-     * Method: Init
-     * -----------------------
      * 初始化
      */
     @Override
     public void init() {
-        makeBall();             // 往屏幕上添加小球
-        makeBricks();
-        addMouseListeners();
-        vx = A;        // 水平速度
-        vy = B;        // 竖直速度
+        makeBall();// 往屏幕上添加小球
+        makeBricks();// 往屏幕上添加砖块
+        makePaddle();// 往屏幕上添加挡板
+        addMouseListeners();// 启用鼠标监听器
+        vx = LEVEL;// 水平速度
+        vy = VERTICAL;// 垂直速度
     }
-
     @Override
     public void run() {
-
         // 等待用户点击
         waitForClick();
-
-        //noinspection InfiniteLoopStatement
-        while (c) {
+        while (stopGame) {
             // 检查是否撞墙
             checkCollision();
-
+            // 检查是否撞砖块
+            checkBricks();
             // 移动小球的位置
             ball.move(vx, vy);
-            we();
-
             // 延迟
             pause(DELAY);
         }
     }
-
     /**
-     * Method: Check Collision
-     * -----------------------
+     * 检查是否撞砖块
+     */
+    void checkBricks() {
+        GObject objColli = getCollidingObject();
+        if (objColli != null) {
+            if (objColli == paddle) {
+                vy = -vy;
+            }
+            else {
+                if (objColli.getColor() == Color.CYAN) {
+                    remove(objColli);
+                    vy = -vy;
+                } else if (objColli.getColor() == Color.GREEN) {
+                    remove(objColli);
+                    vy = -vy;
+                } else if (objColli.getColor() == Color.YELLOW) {
+                    remove(objColli);
+                    vy = -vy;
+                }  else if (objColli.getColor() == Color.BLACK) {
+                        vy = -vy;
+                } else if (objColli.getColor() == Color.ORANGE) {
+                    remove(objColli);
+                    vy = -vy;
+                } else if (objColli.getColor() == Color.RED) {
+                    remove(objColli);
+                    vy = -vy;
+                }
+            }
+        }
+    }
+    /**
      * 检查小球是否和墙相撞，如果相撞，改变小球运动方向
      */
     void checkCollision() {
-        // 小球碰到上下两侧的墙，竖直反弹
+        // 小球碰到上侧的墙，计算3次生命结束游戏；小球碰到下侧的墙，竖直反弹
         if (hitBottomWall()) {
-//            vy = -B;
             if (a < 3) {
+                remove(paddle);
                 remove(ball);
-                GLabel label1 = new GLabel("点击屏幕重新开始");
-                add(label1, 100, 200);
+                clear();
+                JOptionPane.showMessageDialog(null, "点击确定重新开始");
                 a++;
-                makeBall();
+                init();
+                run();
                 waitForClick();
-                remove(label1);
             }else {
-                c = false;
-                GLabel label2 = new GLabel("Game Over");
-                add(label2, 100, 100);
-            }
-            }
-            if (hitTopWall()) {
-                vy = B;
-            }
-
-            // 小球碰到左右两侧的墙，水平反弹
-            if (hitLeftWall()) {
-                vx = A;
-                GLabel label = new GLabel("Game Over");
-            } else if (hitRightWall()) {
-                vx = -A;
+                stopGame = false;
+                JOptionPane.showMessageDialog(null, "Game Over");
             }
         }
-
+        if (hitTopWall()) {
+                vy = VERTICAL;
+        }
+        // 小球碰到左右两侧的墙，水平反弹
+        if (hitLeftWall()) {
+                vx = -LEVEL;
+        } else if (hitRightWall()) {
+                vx = VERTICAL;
+            }
+        }
     /**
-     * Method: Hit Bottom Wall
-     * -----------------------
      * 判断小球是否击中了底部边界
      */
     boolean hitBottomWall() {
-        return ball.getY() >= getHeight() - ball.getHeight();
+        return ball.getY() > getHeight() - ball.getHeight();
     }
-
     /**
-     * Method: Hit Top Wall
-     * -----------------------
      * 判断小球是否击中了顶部边界
      */
     boolean hitTopWall() {
@@ -160,70 +231,75 @@ public int a = 0;
     }
 
     /**
-     * Method: Hit Right Wall
-     * -----------------------
      * 判断小球是否击中了右侧边界
      */
     boolean hitRightWall() {
         return ball.getX() >= getWidth() - ball.getWidth();
     }
-
     /**
-     * Method: Hit Left Wall
-     * -----------------------
      * 判断小球是否击中了左侧边界
      */
     boolean hitLeftWall() {
         return ball.getX() <= 0;
     }
-
     /**
-     * Method: Make Ball
-     * -----------------------
      * 画出一个小球来
      */
     public void makeBall() {
+        // 设置半径
         double size = BALL_RADIUS * 2;
         ball = new GOval(size, size);
-
         // 设置小球为实心
         ball.setFilled(true);
-
-        // 填充颜色是黑色
-        ball.setColor(BALL_COLOR);
-
-        // 添加到画布上(20,20)的位置
+        // 填充颜色为随机颜色
+        ball.setColor(randomGenerator.nextColor());
+        // 添加到画布上(300，500)的位置
         add(ball, 300, 500);
     }
-
+    /**
+     * 画出一堆五颜六色的砖块
+     */
     public void makeBricks() {
-        // 一堆黑色的砖块
-        for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 20; j++) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 30; j++) {
                 GRect brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
                 brick.setFilled(true);
-                brick.setColor(BRICK_COLOR);
+                if (i < 2) {
+                    brick.setColor(Color.RED);
+                } else if (i < 4) {
+                    brick.setColor(Color.ORANGE);
+                } else if (i < 6) {
+                    brick.setColor(Color.YELLOW);
+                } else if (i < 8) {
+                    brick.setColor(Color.GREEN);
+                } else {
+                    brick.setColor(Color.CYAN);
+                }
                 int x = j * (BRICK_MARGIN * 2 + BRICK_WIDTH) + BRICK_MARGIN;
-                int y = 0 + i * (BRICK_MARGIN * 2 + BRICK_HEIGHT) + BRICK_MARGIN;
+                int y = i * (BRICK_MARGIN * 2 + BRICK_HEIGHT) + BRICK_MARGIN;
                 add(brick, x, y);
             }
         }
+    }
+    /**
+     * 画出一个挡板
+     */
+    public void makePaddle(){
+        // 设置挡板长宽
         GRect paddle = new GRect(PADDLE_WIDTH, PADDLE_HEIGHT);
+        // 设置挡板为实心
         paddle.setFilled(true);
-        paddle.setColor(Color.BLUE);
-        add(paddle, 100, 100);
+        // 设置挡板颜色
+        paddle.setColor(Color.BLACK);
+        // 添加挡板到（100，700）
+        add(paddle, 100, 700);
         this.paddle=paddle;
     }
-
-    public void we() {
-        if (getCollidingObject() != null) {
-            vx = -vx;
-            vy = -vy;
-            remove(getCollidingObject());
-        }
-    }
+    /**
+     * 鼠标监听
+     */
     @Override
     public void mouseMoved(MouseEvent e) {
-        paddle.setLocation(e.getX() - paddle.getWidth() / 2, 420);
+        paddle.setLocation(e.getX() - paddle.getWidth() / 2, 700);
     }
 }
